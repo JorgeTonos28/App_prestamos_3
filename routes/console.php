90d75d483d443824f01823bcdc6b9be3f0bc4799
Jorge Tonos <giorgiotonos@gmail.com>
2026-08-07 23:54:27 -0400

    Schedule overdue SMS notifications

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use App\Console\Commands\SendOverdueEmails;
+use App\Console\Commands\SendOverdueSms;
 use App\Console\Commands\UpdateLegalLoans;
 use App\Console\Commands\SendAdminLoanStatusSummary;
 use App\Console\Commands\RunDailyLoanAccruals;
@@ -13,6 +14,9 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command(SendOverdueEmails::class)->dailyAt('08:00');
+Schedule::command(SendOverdueSms::class)
+    ->dailyAt(config('services.labsmobile.overdue_time', '08:05'))
+    ->withoutOverlapping();
 Schedule::command(UpdateLegalLoans::class)->dailyAt('07:30');
 Schedule::command(SendAdminLoanStatusSummary::class)->dailyAt('08:15');
 
